3409cfff0dd2dcc88e63ae44810ca8f7a4ee281c
KingGhidorah01 <martin.andrade@alumnos.udg.mx>
2024-03-11 18:30:37 -0600

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by KingGhidorah01 on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
